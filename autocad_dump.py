613aa8724a1acbba1edb30d23ad7ce1ea1adbde3
Jimmy王 <865026175@qq.com>
2021-02-24 01:13:21 +0800

    1

---
+++ autocad_dump.py
@@ -0,0 +1,45 @@
+import dxfgrabber
+import xlwt
+
+
+def dxf_dumper(dxf1):
+    row = []
+    count = 0
+    for e in dxf1.entities:
+        count += 1
+        if e.dxftype == 'POINT':
+            position = e.point
+            a, b, c = position[0], position[1], position[2]
+            row_list = [e.layer, a, b, c]
+            row.append(row_list)
+    return row
+
+
+def set_stlye(name,height,bold=False):
+    # 初始化样式
+    style = xlwt.XFStyle()
+    font = xlwt.Font()
+    font.bold = bold
+    font.colour_index = 4
+    font.height = height
+    font.name = name
+    style.font = font
+    return style
+
+
+def write_excel(row1):
+    f = xlwt.Workbook()
+    # 创建sheet1
+    sheet1 = f.add_sheet(u'sheet1', cell_overwrite_ok=True)
+    for i in range(len(row1)):
+        for j in range(len(row1[i])):
+            sheet1.write(i, j, row1[i][j], set_stlye("Time New Roman", 220, True))
+    f.save('data.xls')
+    return 0
+
+
+if __name__ == '__main__':
+    dxf = dxfgrabber.readfile("cad\subway0.dxf")
+    row = dxf_dumper(dxf)
+    write_excel(row)
+
