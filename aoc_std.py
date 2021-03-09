b5122b25047a3f11bf172615631544b97ec9840c
“Beast2333” <865026175@qq.com>
2021-03-09 22:58:23 +0800

    1

---
+++ aoc_std.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+
+class ACO:
+    def __init__(self):
+        self.path_data = np.zeros([9, 9], dtype='int')
+        self.path = './path.csv'
+
+    def dump_csv(self):
+        np.savetxt(self.path, self.path_data, delimiter=',')
+        return 0
+
+    def load_csv(self):
+        path_data = np.loadtxt(self.path, delimiter=',')
+        return path_data
+
+
+if __name__ == "__main__":
+    ACO_demo = ACO()
+    # ACO_demo.dump_csv()
+    ACO_demo.path_data = ACO_demo.load_csv()
+    print(ACO_demo.path_data)
+
+
